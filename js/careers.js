670c67a70d783a0b59ad5b7b0080de2e0ed11e1b
BanchaTNK <67319010047@tnk.ac.th>
2025-06-25 08:47:46 +0700

    ครั่งที่1

---
+++ js/careers.js
@@ -0,0 +1,20 @@
+const careers = [
+  {
+    id: "web-dev",
+    name: "Web Developer",
+    description: "พัฒนาเว็บไซต์ทั้งฝั่งหน้าและหลังบ้าน",
+    skills: ["HTML", "CSS", "JavaScript", "Git", "React"],
+    salary: "18,000 - 40,000 บาท",
+    path: "เรียนสาย IT / เรียนรู้จากคอร์สออนไลน์",
+    image: "img/web-dev.jpg"
+  },
+  {
+    id: "data-analyst",
+    name: "Data Analyst",
+    description: "วิเคราะห์ข้อมูลเพื่อช่วยการตัดสินใจขององค์กร",
+    skills: ["Excel", "SQL", "Power BI", "Python"],
+    salary: "22,000 - 60,000 บาท",
+    path: "เรียนสายข้อมูล / Data Science",
+    image: "img/data-analyst.jpg"
+  }
+];
